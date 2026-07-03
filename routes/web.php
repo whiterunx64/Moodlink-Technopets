a784acf614dab6be9b1b3faee44465b6b5dfd419
whiterunx64 <tadilimichael2@protonmail.com>
2026-07-03 12:23:04 +0800

    Middleware back

--- routes/web.php
+++ routes/web.php
@@ -133,7 +133,7 @@ Route::get('/test-password', function () {
     return collect(range(1, 20))->map(fn() => $m->invoke($svc));
 });
 
-//Route::middleware(['auth', 'supabase.verify-token', 'supabase.require-admin-access', 'supabase.single-session'])->group(function () {
+Route::middleware(['auth', 'supabase.verify-token', 'supabase.require-admin-access', 'supabase.single-session'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
@@ -205,7 +205,7 @@ Route::get('/test-password', function () {
     Route::delete('/profile/admin', [ProfileController::class, 'destroyAccount'])
         ->middleware('supabase.revalidate')
         ->name('profile.account.destroy');
-//});
+});
 
 Route::get('/cron/run', function (Request $request) {
     $cronKey = config('app.cron_key');
